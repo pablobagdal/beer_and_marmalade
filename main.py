af2f06651fbe214ad6a5a2f7ff8855ae4bba5704
pablobagdal <pablobagdal@gmail.com>
2023-11-27 00:01:24 +0300

    finish with main part

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 import gspread as gs
 import pandas as pd
-import threading
+
 from links import *
 from functions import *
-import time
 
-gc = None
 filename = 'service_account.json'
 
-
-def my_caller():
-    global gc
-    gc = gs.service_account(filename)
-
-
 if __name__ == '__main__':
-    # new_thread = threading.Thread(target=my_caller)
-    # new_thread.start()
-
     # page_num = int(input('Введите номер страницы: '))
-    # item_num = int(input('Введите номер предмета: ')) - 1
-    
-    time.sleep(3)
-    page_num = 1
-    item_num = 2
+    item_num = int(input('Введите номер предмета: ')) - 1
 
-    new_thread.join()
-
-    # gc = gs.service_account(filename='service_account.json')
+    gc = gs.service_account(filename='service_account.json')
 
     sh = gc.open_by_url(table_url)
 
@@ -37,7 +20,10 @@ if __name__ == '__main__':
     df.head()
 
     # Пример использования
-
-
     result_text = generate_text(df.iloc[item_num])
+    print()
+    print()
+    print()
     print(result_text)
+    print()
+    print()
